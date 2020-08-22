49282baf8df4df030a950d078283ff6eb2a83a87
Claudiu-George Cudla <claudiu.cudla@osf.digital>
2020-08-21 22:29:56 -0400

    Updates

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 var animSpeed = 200;
 
 $(document).ready(function(){
-    $("button[name='next']").on("click", function(event) {
-        var $step = $(this).parents(".step");
-        var $stepBody = $step.find(".body");
-        $stepBody.slideUp(animSpeed, "swing");
+    // $("button[name='next']").on("click", function(event) {
+    //     var $step = $(this).parents(".step");
+    //     var $stepBody = $step.find(".body");
+    //     $stepBody.slideUp(animSpeed, "swing");
 
-        var $nextStep = $step.next(".step");
-        if ($nextStep.length) {
-            $nextStep.find(".body").slideDown(animSpeed, "swing");
-        }
-    });
-    $(".step-heading").on("click", function(event) {
-        var $step = $(this).parents(".step");
-        var $stepBody = $step.find(".body");
-        $stepBody.slideToggle(animSpeed, "swing");
-    });
+    //     var $nextStep = $step.next(".step");
+    //     if ($nextStep.length) {
+    //         $nextStep.find(".body").slideDown(animSpeed, "swing");
+    //     }
+    // });
+    // $(".step-heading").on("click", function(event) {
+    //     var $step = $(this).parents(".step");
+    //     var $stepBody = $step.find(".body");
+    //     $stepBody.slideToggle(animSpeed, "swing");
+    // });
 
     $(".copy-all-results").on("click", function() {
         copyHTML($("#output")[0]);
