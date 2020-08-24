0022b6ba98091b6013bd2d2ec6ea51263a484329
Claudiu-George Cudla <claudiu.cudla@osf.digital>
2020-08-23 21:37:18 -0400

    Added countries logic with copy

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
 var animSpeed = 200;
 
-$(document).ready(function(){
+$(document).ready(function() {
+    $(".step-toggle").on("change", function(event) {
+        var $this = $(this);
+        var $step = $this.parents(".page__toggle").find(".input-expand");
+        if ($this.prop("checked")) {
+            $step.slideDown(300, "swing");
+        } else {
+            $step.slideUp(300, "swing");
+        }
+    });
+
+    $(".copy-from-above").on("click", function(event) {
+        event.preventDefault();
+        var $this = $(this);
+        var $currQ = $this.parents(".question");
+        var $prevQ = $currQ.prev();
+
+        // Handle the case when the current question is on the next step
+        if ($prevQ.length == 0) {
+            $prevQ = $currQ.parents(".step").prev().find(".question").last();
+        }
+
+        if ($prevQ.length) {
+            var $currCountries = $currQ.find("input.country-select");
+            var $prevCountries = $prevQ.find("input.country-select");
+
+            for (var i = 0; i < $prevCountries.length; i++) {
+                $($currCountries[i]).prop("checked", $($prevCountries[i]).prop("checked"));
+            }
+        }
+    });
+
     // $("button[name='next']").on("click", function(event) {
     //     var $step = $(this).parents(".step");
     //     var $stepBody = $step.find(".body");
